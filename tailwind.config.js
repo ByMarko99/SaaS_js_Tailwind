36d727a648b5f95d4a6aa396281fff30fc30b74c
Marko Gros <markogrostx@gmail.com>
2023-12-05 14:13:05 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,12 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-animated'),
+
+  ],
+
 }
